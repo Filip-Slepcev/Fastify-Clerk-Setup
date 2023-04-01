0536e3236c8119fc14adefbf2ec3f8abca3faeb8
Filip-Slepcev <filipslepcev@yahoo.ca>
2023-04-01 18:37:36 -0400

    uploading files

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,12 @@
 import "./loadEnv";
-
 import Fastify from "fastify";
 import { clerkClient, clerkPlugin, getAuth } from "@clerk/fastify";
+import routes from "./routes/chat";
 
-const fastify = Fastify({ logger: true });
+const fastify = Fastify();
 
-/**
- * Register the Clerk plugin globally.
- * By default, Clerk will initialise using the API keys from the environment if found.
- *
- * If you prefer to pass the keys to the plugin explicitly, see `src/using-runtime-keys.ts`
- * If you prefer to register the plugin for specific routes only, see `src/authenticating-specific-routes.ts`
- */
 fastify.register(clerkPlugin);
-
-fastify.get("/", async (req, reply) => {
-  const { userId } = getAuth(req);
-  const user = userId ? await clerkClient.users.getUser(userId) : null;
-  return { user };
-});
+fastify.register(routes, { prefix: "/chat" });
 
 const start = async () => {
   try {
@@ -28,5 +16,6 @@ const start = async () => {
     process.exit(1);
   }
 };
-
 start();
+
+export default fastify;
